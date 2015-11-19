6791e6eb1c1dbbe0583d6ec09b3e6643f40d07a0
Andru Lin <Andru Lin>
2015-11-18 21:26:47 -0500

    PHASE 2 COMPLETE

--- Rational.java
+++ Rational.java
@@ -42,11 +42,49 @@ public class Rational {
         this.numerator *= other.denominator;
         this.denominator *= other.numerator;
     }
-    
+
+    //======================PHASE2==================================//
+    public static int gcdER (int a, int b){
+        if ((a == b) || (b == 0)){
+            return a;} //returns the GCD.
+        else if (a < b){
+            return gcdER (b,a);} //If b is greater than a, the function will be run again with both values swapped.
+        else{
+            return gcdER (b , (a-b));
+        }
+    }
+    //Adds fractions brute-force way
+    public void add (Rational other) {
+	this.numerator = (this.numerator)*(other.denominator) + (other.numerator)*(this.denominator);
+	this.denominator = (this.denominator*other.denominator); }
+
+    //subtracts fractions brute-force way
+    public void subtract (Rational other) {
+	this.numerator = (this.numerator*other.denominator) - (other.numerator*this.denominator);
+	this.denominator = (this.denominator*other.denominator); }
+
+    //reduces fraction
+    public void reduce () {
+	int gcd = gcdER (this.numerator, this.denominator);
+	this.numerator /= gcd;
+	this.denominator /= gcd; }
+
+    //========================PHASE3================================//
+
     public static void main(String[] args) {
         Rational r = new Rational(2, 3);
         Rational s = new Rational(1, 2);
         r.divide(s);
         System.out.println(r);
+	r.multiply(s);
+        System.out.println(r);
+	//Phase 2 Test
+	r.add(s);
+	System.out.println(r);
+	r.subtract(s);
+	System.out.println(r);
+	r.reduce();
+	System.out.println(r);
+	//Phase3 Test
     }
 }
